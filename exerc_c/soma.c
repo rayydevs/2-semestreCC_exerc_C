59f5b7539b9f4fd2eddc6acfedc398f80e1ce5c5
Rayane Souza <rayanecoelho1504@gmail.com>
2024-12-04 11:19:17 -0300

    soma.c

--- exerc_c/soma.c
+++ exerc_c/soma.c
@@ -3,17 +3,17 @@
 
 int main(){
 	setlocale(LC_ALL,"portuguese");
-	int valor1, valor2, soma; //declara variável
+	int valor1, valor2, soma; //declara variÃ¡vel
 	
 	printf("Digite o primeiro valor:"); //exibe a mensagem na tela
-	scanf("%d", &valor1); //lê o valor inteiro e indica onde deve ser armazenado
+	scanf("%d", &valor1); //recebe o valor
 	
 	printf("Digite o segundo valor:");
 	scanf("%d", &valor2);
 	
 	soma = valor1 + valor2; //soma os valores
 	
-	printf("A soma de %d e %d é %d\n", valor1, valor2, soma); //exibe o resultado da soma na tela
+	printf("A soma de %d e %d Ã© %d\n", valor1, valor2, soma); //exibe o resultado da soma na tela
 	
 	return 0; //programa finalizado
 }
