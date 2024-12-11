d962c6da633ae1a7ecc67c23d6567f37264d71c8
Rayane Souza <rayanecoelho1504@gmail.com>
2024-12-11 19:11:11 -0300

    Update and rename exerc22.c to switch.c

--- exerc_c/switch.c
+++ exerc_c/switch.c
@@ -1,7 +1,7 @@
 /*
-Uma loja fornece 10% de desconto para funcion·rios e 5% de desconto para clientes vips. FaÁa um
-programa que calcule o valor total a ser pago por uma pessoa. O programa dever· ler o valor total da
-compra efetuada e um cÛdigo que identifique se o comprador È um cliente comum (1), funcion·rio (2)
+Uma loja fornece 10% de desconto para funcion√°rios e 5% de desconto para clientes vips. Fa√ßa um
+programa que calcule o valor total a ser pago por uma pessoa. O programa dever√° ler o valor total da
+compra efetuada e um c√≥digo que identifique se o comprador √© um cliente comum (1), funcion√°rio (2)
 ou vip (3).
 */
 
@@ -12,9 +12,9 @@ int main(){
 	float valor_p, valor_total;
 	printf("Digite o valor do seu produto:");
 	scanf("%f", &valor_p);
-	printf("Digite qual opÁ„o se encaixa no seu perfil:");
+	printf("Digite qual op√ß√£o se encaixa no seu perfil:");
 	printf("(1) cliente comum:");
-	printf("(2) funcion·rio:");
+	printf("(2) funcion√°rio:");
 	printf("(3) vip:");
 	scanf("%d", &x);
 	
@@ -26,7 +26,7 @@ int main(){
 		case 3: valor_total = valor_p-(valor_p*0);
 		break;
 		default:
-			printf("opÁ„o inv·lida.");
+			printf("op√ß√£o inv√°lida.");
 	}
 	printf("O valor da sua compra foi de R$%.0f", valor_total);
 }
